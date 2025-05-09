26102c7e030c48f81e884860e9111f9ef74f28f1
vbelhomm <valentinbelhomme@gmail.com>
2025-05-09 16:03:06 +0200

    feat: routes and first version og header.

---
+++ routes.js
@@ -0,0 +1,28 @@
+const routes = [
+    {
+        path: '/',
+        templateUrl: './pages/home.html'
+    },
+    {
+        path: '/contact-us',
+        templateUrl: './pages/contact.html'
+    },
+    {
+        path: '/bio',
+        templateUrl: './pages/bio.html'
+    },
+    {
+        path: '/musica',
+        templateUrl: './pages/musica.html'
+    },
+    {
+        path: '/video',
+        templateUrl: './pages/video.html'
+    },
+    {
+        path: '/botiga',
+        templateUrl: './pages/botiga.html'
+    },
+]
+// remplacer la template par un fichier html
+export default routes
